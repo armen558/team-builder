f474d921f7f8f92f6169cde2181a3d83c6885e88
armen558 <armenpetrosyan665@gmail.com>
2020-07-20 18:03:25 +0400

    initial subtree commit

---
+++ precache-manifest.5141ebb880ea7fcef3b360626867d427.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4622f20f33483141b2d9f0a900122635",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24715ff2626981503962",
+    "url": "/static/css/main.9c23e2fd.chunk.css"
+  },
+  {
+    "revision": "03cf68d3804ec9181a19",
+    "url": "/static/js/2.b9ceb4c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b9ceb4c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24715ff2626981503962",
+    "url": "/static/js/main.98f47595.chunk.js"
+  },
+  {
+    "revision": "8b8f8712f7998908b0a9",
+    "url": "/static/js/runtime-main.8c44053c.js"
+  }
+]);
